fd1d2d91359c236d263bcfa690d2b79e02707a89
Rodrous <59244346+Rodrous@users.noreply.github.com>
2022-07-15 00:00:42 +0530

    Inital Commit

---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+## Prequisites
+- Python 3.10 or later
+### How to run
+- First install requirements using command-line
+    ``` pip install -r requirements.txt ```
+Create profiles in "main" function and call the solution inside the results variable
+
+- Run the code
--- Solution.py
+++ Solution.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass, field
 import datetime
 from typing import Optional, List, Dict
 from thefuzz import fuzz
+import asyncio
 
 
 @dataclass
@@ -14,42 +15,51 @@ class Profile:
 
 
 class Solution:
-
-    def find_duplicates(self, profiles: List[Profile], fields: List[str]) -> Dict:
+    @staticmethod
+    async def find_duplicates(profiles: List[Profile], fields: List[str]) -> Dict | str:
         result_dict: Dict = {}
         total_score: int = 0
-        common_attributes: List[str] = []
-
+        common_attributes: Optional[List[str]] = []
+        ignored_attributes: Optional[List[str]] = []
         for attributes in fields:
             if attributes == "class_year":
-                if profiles[0].class_year == profiles[1].class_year and profiles[0].class_year == profiles[
-                    1].class_year:
-                    common_attributes.append(attributes)
-                    total_score += 1
-                elif profiles[0].class_year == profiles[1].class_year and profiles[0].class_year != profiles[
-                    1].class_year:
-                    total_score -= 1
+                if profiles[0].class_year and profiles[1].class_year:
+
+                    if profiles[0].class_year == profiles[1].class_year and profiles[0].class_year == profiles[
+                        1].class_year:
+                        common_attributes.append(attributes)
+                        total_score += 1
+                    elif profiles[0].class_year == profiles[1].class_year and profiles[0].class_year != profiles[
+                        1].class_year:
+                        total_score -= 1
+                else:
+                    ignored_attributes.append("class_year")
 
             if attributes == "date_of_birth":
-                if profiles[0].date_of_birth == profiles[1].date_of_birth and profiles[0].date_of_birth == profiles[
-                    1].date_of_birth:
-                    common_attributes.append(attributes)
-                    total_score += 1
-                elif profiles[0].date_of_birth == profiles[1].date_of_birth and profiles[0].date_of_birth != profiles[
-                    1].date_of_birth:
-                    total_score -= 1
+                if profiles[0].date_of_birth and profiles[1].date_of_birth:
+                    if profiles[0].date_of_birth == profiles[1].date_of_birth and profiles[0].date_of_birth == profiles[
+                        1].date_of_birth:
+                        common_attributes.append(attributes)
+                        total_score += 1
+                    elif profiles[0].date_of_birth == profiles[1].date_of_birth and profiles[0].date_of_birth != \
+                            profiles[
+                                1].date_of_birth:
+                        total_score -= 1
+                else:
+                    ignored_attributes.append("date_of_birth")
 
         # If fields contains all ["first_name", "last_name", "email_field"]
         if all(attribute in fields for attribute in ["first_name", "last_name", "email_field"]):
             if fuzz.ratio(profiles[0].first_name, profiles[1].first_name) > 80 and fuzz.ratio(profiles[0].last_name,
                                                                                               profiles[
                                                                                                   1].last_name) > 80 and fuzz.ratio(
-                    profiles[0].email_field, profiles[1].email_field) > 80:
+                profiles[0].email_field, profiles[1].email_field) > 80:
                 total_score += 1
                 common_attributes.append("first_name")
                 common_attributes.append("last_name")
                 common_attributes.append("email_field")
         else:
+
             for attribute in fields:
                 if attribute == "first_name":
                     if fuzz.ratio(profiles[0].first_name, profiles[1].first_name) > 80:
@@ -70,26 +80,26 @@ class Solution:
         if total_score >= 1:
             non_common_attributes = set(fields) ^ set(common_attributes) if set(fields) ^ set(
                 common_attributes) else None
-            elements_profile_1 = set(profiles[0].__dict__.keys())
-            elements_profile_2 = set(profiles[1].__dict__.keys())
-            total_attributes = elements_profile_1.union(elements_profile_2)
-
+            if not ignored_attributes:
+                ignored_attributes = None
             result_dict["Profiles"] = profiles
             result_dict["total_match_score"] = total_score
-            result_dict["matching_attributes"] = common_attributes.sort()
+            result_dict["matching_attributes"] = common_attributes
             result_dict["non_matching_attributes"] = non_common_attributes
-            # result_dict["ignored_attributes"] = ignored_attributes
+            result_dict["ignored_attributes"] = ignored_attributes
+        else:
+            return "Not Matching"
 
         return result_dict
 
 
-if __name__ == "__main__":
+async def main():
     profile_1 = Profile(
         email_field="knowkanhai@gmail.com",
         first_name="Kanhai",
         last_name="Shah",
-        class_year=None,
-        date_of_birth=None
+        class_year=2012,
+        date_of_birth=datetime.date(1990, 10, 11)
     )
 
     profile_2 = Profile(
@@ -100,4 +110,31 @@ if __name__ == "__main__":
         date_of_birth=datetime.date(1990, 10, 11)
     )
 
-    Solution().find_duplicates([profile_1, profile_2], ["first_name", "last_name", "email_field"])
+    profile_3 = Profile(
+        email_field="knowkanhai@gmail.com",
+        first_name="Kanhai",
+        last_name="Shah",
+        class_year=None,
+        date_of_birth=None
+    )
+
+    profile_4 = Profile(
+        email_field="knowkanhai@gmail.com",
+        first_name="Kanhai1",
+        last_name="Shah",
+        class_year=2012,
+        date_of_birth=datetime.date(1990, 10, 11)
+    )
+    solutionObj = Solution()
+    results = await asyncio.gather(
+        solutionObj.find_duplicates(profiles=[profile_1, profile_2], fields=["first_name", "last_name"]),
+        solutionObj.find_duplicates(profiles=[profile_3,profile_4], fields=["first_name", "last_name","email_field", "class_year", "dete_of_birth"])
+
+
+    )
+
+    return results
+
+
+if __name__ == "__main__":
+    print(asyncio.run(main()))
